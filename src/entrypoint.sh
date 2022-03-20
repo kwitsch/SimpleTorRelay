437ee0628f964081411fa18f99c1fda812b8e33e
Kwitsch <kwitsch@gmail.com>
2022-03-20 13:57:59 +0100

    TOR_ADDRESS fix

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -8,10 +8,6 @@ if [ ! -z "$TOR_CONTACTINFO" ]; then
     printf "\nContactInfo ${TOR_CONTACTINFO}" >> /torrc
 fi
 
-if [ ! -z "$TOR_ADDRESS" ]; then
-    printf "\nAddress ${TOR_ADDRESS}" >> /torrc
-fi
-
 if [ ! -z "$TOR_RELAYBANDWIDTHRATE" ]; then
     printf "\nRelayBandwidthRate ${TOR_RELAYBANDWIDTHRATE}" >> /torrc
 fi
@@ -19,5 +15,15 @@ fi
 if [ ! -z "$TOR_RELAYBANDWIDTHBURST" ]; then
     printf "\nRelayBandwidthBurst ${TOR_RELAYBANDWIDTHBURST}" >> /torrc
 fi
+
+if [ ! -z "$TOR_ADDRESS" ]; then
+    printf "\nORPort ${TOR_ADDRESS}:9001 IPv4Only" >> /torrc
+    printf "\nDirPort ${TOR_ADDRESS}:9030 IPv4Only" >> /torrc
+else
+    printf "\nORPort 9001 IPv4Only" >> /torrc
+    printf "\nDirPort 9030 IPv4Only" >> /torrc
+fi
+
+printf "\n" >> /torrc
 
 exec tor -f /torrc
